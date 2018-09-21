35df6bd1967e00be507838cf02bdc5a9025b33f9
Rio Mukhtarom <riomukhtarom10@gmail.com>
2018-09-21 18:05:38 +0800

    fixed main and details

---
+++ app/src/main/java/com/gdk/rio/footballschedule/model/match/Match.kt
@@ -0,0 +1,73 @@
+package com.gdk.rio.footballschedule.model.match
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Match (
+        @SerializedName("dateEvent")
+        var matchDate: String? = null,
+
+        //home
+        @SerializedName("idHomeTeam")
+        var homeId: String? = null,
+
+        @SerializedName("strHomeTeam")
+        var homeTeam: String? = null,
+
+        @SerializedName("intHomeScore")
+        var homeScore: String? = null,
+
+        @SerializedName("strHomeFormation")
+        var homeFormation: String? = null,
+
+        @SerializedName("strHomeGoalDetails")
+        var homeGoalDetails: String? = null,
+
+        @SerializedName("intHomeShots")
+        var homeShots: String? = null,
+
+        @SerializedName("strHomeLineupGoalkeeper")
+        var homeGoalKeeper: String? = null,
+
+        @SerializedName("strHomeLineupDefense")
+        var homeDefense: String? = null,
+
+        @SerializedName("strHomeLineupMidfield")
+        var homeMidfield: String? = null,
+
+        @SerializedName("strHomeLineupForward")
+        var homeForward: String? = null,
+
+        //away
+        @SerializedName("idAwayTeam")
+        var awayId: String? = null,
+
+        @SerializedName("strAwayTeam")
+        var awayTeam: String? = null,
+
+        @SerializedName("intAwayScore")
+        var awayScore: String? = null,
+
+        @SerializedName("strAwayFormation")
+        var awayFormation: String? = null,
+
+        @SerializedName("strAwayGoalDetails")
+        var awayGoalDetails: String? = null,
+
+        @SerializedName("intAwayShots")
+        var awayShots: String? = null,
+
+        @SerializedName("strAwayLineupGoalkeeper")
+        var awayGoalKeeper: String? = null,
+
+        @SerializedName("strAwayLineupDefense")
+        var awayDefense: String? = null,
+
+        @SerializedName("strAwayLineupMidfield")
+        var awayMidfield: String? = null,
+
+        @SerializedName("strAwayLineupForward")
+        var awayForward: String? = null
+) : Parcelable
---
+++ app/src/main/java/com/gdk/rio/footballschedule/model/match/MatchResponse.kt
@@ -0,0 +1,3 @@
+package com.gdk.rio.footballschedule.model.match
+
+data class MatchResponse (val events: List<Match>)
